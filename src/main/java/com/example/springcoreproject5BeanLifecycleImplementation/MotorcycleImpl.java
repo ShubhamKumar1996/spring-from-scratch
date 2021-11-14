dfa10f53306ffa9d92c46c76d577982a998d715e
ShubhamKumar1996 <Nimcetshubhamkumar@gmail.com>
2021-11-14 07:29:44 +0530

    Implementing lifecycle methods of Spring Bean using XML

---
+++ src/main/java/com/example/springcoreproject5BeanLifecycleImplementation/MotorcycleImpl.java
@@ -0,0 +1,14 @@
+package com.example.springcoreproject5BeanLifecycleImplementation;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MotorcycleImpl {
+    /* Implementing lifecycle methods of Spring Bean using XML */
+    public static void main(String[] args) {
+        AbstractApplicationContext context = new ClassPathXmlApplicationContext("MotorcycleConfig.xml");
+        Motorcycle motorcycle1 = (Motorcycle) context.getBean("motorcycle1");
+        System.out.println(motorcycle1);
+        context.registerShutdownHook(); /* for calling destroy method */
+    }
+}
